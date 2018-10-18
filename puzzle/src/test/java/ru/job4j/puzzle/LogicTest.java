80a388b44d2472c11adf9f801f605de7723865f0
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-10-18 13:25:14 +0300

    Initial commit

---
+++ puzzle/src/test/java/ru/job4j/puzzle/LogicTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.puzzle;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Testing the method for determining the victory in the game.
+ * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
+ * @version 1.0.
+ * @since 18.10.2018.
+ */
+public class LogicTest {
+    @Test
+    public void whenVerticalWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(true));
+    }
+
+    @Test
+    public void whenHorizontalWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(true));
+    }
+
+    @Test
+    public void whenNotWin() {
+        Logic logic = new Logic(5) {
+            @Override
+            public int[][] convert() {
+                return new int[][] {
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {1, 1, 0, 1, 1},
+                        {0, 0, 1, 0, 0},
+                        {0, 0, 1, 0, 0},
+                };
+            }
+        };
+        assertThat(logic.isWin(), is(false));
+    }
+}
